4cb0081218871942775ff0d1da3841f72877c778
pj-022 <piyushjha524@gmail.com>
2022-12-14 08:11:36 +0530

    Initial commit

---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    # return HttpResponse("This is my First Django Homepage")
+    return render(request, 'index.html')
+
+def about(request):
+    # return HttpResponse("This is my First Django Homepage")
+    return render(request, 'about.html')
+
+def services(request):
+    # return HttpResponse("This is my First Django Homepage")
+    return render(request, 'services.html')
